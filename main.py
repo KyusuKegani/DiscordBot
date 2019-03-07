1c12663cb2137133083f24760ac6c10aa947217d
Yusuke Sabi <nekomastery050313@gmail.com>
2019-03-07 18:00:48 +0900

    Initial Commit

---
+++ main.py
@@ -0,0 +1,27 @@
+import discord
+import settings
+
+BOT_TOKEN = settings.BT
+client = discord.Client()
+
+
+@client.event
+async def on_ready():
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('------')
+
+
+@client.event
+async def on_message(message):
+    # 「おはよう」で始まるか調べる
+    if message.content.startswith("おはよう"):
+        # 送り主がBotだった場合反応したくないので
+        if client.user != message.author:
+            # メッセージを書きます
+            m = "おはようでし" + message.author.name + "！"
+            # メッセージが送られてきたチャンネルへメッセージを送ります
+            await client.send_message(message.channel, m)
+
+client.run(BOT_TOKEN)

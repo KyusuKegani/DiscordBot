18a0d2dea274ab18d21965e80372e073f08cfca7
Yusuke Sabi <nekomastery050313@gmail.com>
2019-04-04 13:32:49 +0900

    パーミッションの関係上一旦スクリプトを削除

--- scripts/ApplicationStart.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-pkill -SIGKILL -f 'python main.py'
-cd /home/ec2-user/discord/DiscordBot/
-python main.py &
--- scripts/BeforeInstall.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-rm -rf /home/ec2-user/discord/DiscordBot/
-mkdir /home/ec2-user/discord/DiscordBot/
-sudo cp /home/ec2-user/resources/.env /home//ec2-user/discord/DiscordBot/
